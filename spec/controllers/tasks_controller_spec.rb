8020f74d5c69cbb5c653109ce0554bdc40e5d018
Your Name <tantohyung@gmail.com>
2016-07-02 00:32:34 +0000

    Successful form submission

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -30,4 +30,14 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "tasks#create" do 
+    it 'should create a new task in the database' do 
+      post :create, task: {title: 'Fix things'}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq ("Fix things")
+    end
+  end
+
 end
